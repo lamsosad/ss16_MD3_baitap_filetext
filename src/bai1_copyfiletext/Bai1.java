4a7339ab602bc19f653246f3993461841fbaf888
Lâm Nguyễn Bá <nguyenbalam2307@gmail.com>
2023-03-31 09:24:36 +0700

    up

--- src/bai1_copyfiletext/Bai1.java
+++ src/bai1_copyfiletext/Bai1.java
@@ -8,14 +8,15 @@ public class Bai1 {
         try {
             File file = new File("src/bai1_copyfiletext/source.txt");
             FileReader fileReader = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fileReader);
-
-            while ((line = reader.readLine()) != null) {
-                FileWriter writer = new FileWriter("src/bai1_copyfiletext/target.txt");
-                writer.write(line);
-                writer.close();
+            BufferedReader br = new BufferedReader(fileReader);
+            FileWriter writer = new FileWriter("src/bai1_copyfiletext/target.txt");
+            BufferedWriter bw = new BufferedWriter(writer);
+            while ((line = br.readLine()) != null) {
+                bw.write(line);
+                bw.newLine();
             }
-            reader.close();
+            bw.close();
+            br.close();
         } catch (IOException i) {
             System.out.println("Lỗi xảy ra");
         }
